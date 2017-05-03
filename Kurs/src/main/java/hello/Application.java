90bc362c44bd33434da6e8b466362c2596c87383
Anna <annazahk15@gmail.com>
2017-05-03 20:48:07 +0300

    Init

---
+++ Kurs/src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by Анютка on 03.05.2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ Kurs/src/main/java/hello/model/TipIgrushek.java
@@ -0,0 +1,35 @@
+package hello.model;
+import javax.persistence.*;
+
+/**
+ * Created by Анютка on 03.05.2017.
+ */
+@Entity
+@Table(name="tip igrushek")
+        public class TipIgrushek {
+        @Id
+        @GeneratedValue(strategy= GenerationType.AUTO)
+        private Integer id;
+        }
+        @Column(name="n_tipa")
+         private int n_tipa;
+         private int tip_igrushki;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public int getN_tipa(){
+        return n_tipa;
+    }
+  public void setN_tipa(int n_tipa) {
+      this.n_tipa = n_tipa;
+  }
+ public int getTip_igrushki() {
+     return tip_igrushki;
+ }
+ public void setTip_igrushki(int tip_igrushki){
+        this.tip_igrushki = tip_igrushki;
+ }
+}
