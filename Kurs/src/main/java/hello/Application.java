6b55910998f6a838eb671f1dabc3b3732b161acb
Anna <annazahk15@gmail.com>
2017-05-14 10:31:12 +0300

    Kurs

--- Kurs/src/main/java/hello/Application.java
+++ Kurs/src/main/java/hello/Application.java
@@ -3,13 +3,26 @@ package hello;
 /**
  * Created by Анютка on 03.05.2017.
  */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+@ComponentScan
+@EnableAutoConfiguration
+//@SpringBootApplication
 public class Application {
+    @Autowired
+    private TipIgrushekRepository tipIgrushekRepository;
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 }
